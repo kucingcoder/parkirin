9a9af6253ce48631a4e9fec0382be071afdf057d
kucingcoder <137396727+kucingcoder@users.noreply.github.com>
2023-12-26 22:56:44 +0700

    menambahkan fungsi login

--- src/main/java/com/m/ibrahimhanif/Controller/data_adapter.java
+++ src/main/java/com/m/ibrahimhanif/Controller/data_adapter.java
@@ -2,33 +2,30 @@ package com.m.ibrahimhanif.Controller;
 import com.m.ibrahimhanif.Model.basis_data;
 import com.m.ibrahimhanif.Model.pengaturan;
 import java.sql.ResultSet;
-import javax.swing.table.DefaultTableModel;
 
 public class data_adapter {
     public static pengaturan settingan;
-    public static basis_data database;
+    public static basis_data database = new basis_data();
+    
+    public class akun { public static String uuid, level; }
     
     public static void MuatPengaturan() throws Exception{
         settingan = new pengaturan();
         database.Hubungkan(settingan.getKoneksiDatabase());
     }
     
-    public static void PerbaharuiDataTabel(String tabel, String manipulasi, String kriteria) throws Exception{
-        database.SetData("UPDATE " + tabel + " " + manipulasi + " WHERE " + kriteria);
-    }
-    
-    public static DefaultTableModel AmbilDataTabel(String tabel, String data, String kriteria) throws Exception{
-        ResultSet hasil = database.AmbilData("SELECT " + data + " FROM " + tabel + " WHERE " + kriteria);
-        DefaultTableModel model = new DefaultTableModel(); 
-        
-        for (int i = 1; i <= hasil.getMetaData().getColumnCount(); i++) { model.addColumn(hasil.getMetaData().getColumnName(i)); }
-            
-        while (hasil.next()) {
-            Object[] rowData = new Object[hasil.getMetaData().getColumnCount()];
-            for (int i = 1; i <= hasil.getMetaData().getColumnCount(); i++) { rowData[i - 1] = hasil.getObject(i); }
-            model.addRow(rowData);
+    public static boolean Masuk(String id, String sandi) {
+        try {
+            ResultSet rs;
+            rs = database.AmbilData("SELECT uuid, level FROM pegawai WHERE id = '" + id + "' and sandi = MD5('" + sandi + "')");
+            if (rs.next()) {
+                akun.uuid = rs.getString("uuid");
+                akun.level = rs.getString("level");
+                return true;
+            } else { return false; }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return false;
         }
-        
-        return model;
     }
 }
