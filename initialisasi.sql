798a73674366d0926e0761916794b2307542500f
kucingcoder <137396727+kucingcoder@users.noreply.github.com>
2023-12-31 20:05:20 +0700

    Ganti struktur tabel

--- initialisasi.sql
+++ initialisasi.sql
@@ -1,31 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1deb5ubuntu1
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Generation Time: Dec 28, 2023 at 11:25 AM
--- Server version: 10.6.12-MariaDB-0ubuntu0.22.04.1
--- PHP Version: 8.1.2-1ubuntu2.14
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `parkirin`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `pegawai`
---
 
 CREATE TABLE `pegawai` (
   `uuid` char(32) NOT NULL,
@@ -38,18 +13,8 @@ CREATE TABLE `pegawai` (
   `level` enum('Standar','Admin') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `pegawai`
---
-
 INSERT INTO `pegawai` (`uuid`, `id`, `sandi`, `nama`, `gender`, `telp`, `alamat`, `level`) VALUES
-('1ac82eb3644ea8f3ef0b4e9322e59cba', 'admin', '21232f297a57a5a743894a0e4a801fc3', 'Default Admin', 'Laki - Laki', '-', '-', 'Admin');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `pemarkiran`
---
+('1ac82eb3644ea8f3ef0b4e9322e59cba', 'admin', '21232f297a57a5a743894a0e4a801fc3', 'Admin', 'Laki - Laki', '-', '-', 'Admin');
 
 CREATE TABLE `pemarkiran` (
   `no` int(11) NOT NULL,
@@ -58,64 +23,32 @@ CREATE TABLE `pemarkiran` (
   `masuk` datetime NOT NULL,
   `keluar` datetime DEFAULT NULL,
   `status` enum('Normal','Bayar Denda') DEFAULT NULL,
-  `biaya` int(11) DEFAULT NULL,
+  `total_biaya` int(11) DEFAULT NULL,
   `pegawai` varchar(120) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tarif`
---
 
 CREATE TABLE `tarif` (
   `id` int(11) NOT NULL,
   `jenis` varchar(120) NOT NULL,
-  `hari_pertama` int(11) NOT NULL,
-  `per_jam` int(11) NOT NULL
+  `waktu_normal` int(11) NOT NULL,
+  `biaya_normal` int(11) NOT NULL,
+  `biaya_perjam` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `pegawai`
---
 ALTER TABLE `pegawai`
   ADD PRIMARY KEY (`uuid`),
   ADD UNIQUE KEY `nomor_pegawai` (`id`(64));
 
---
--- Indexes for table `pemarkiran`
---
 ALTER TABLE `pemarkiran`
   ADD PRIMARY KEY (`no`);
 
---
--- Indexes for table `tarif`
---
 ALTER TABLE `tarif`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `jenis` (`jenis`);
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `pemarkiran`
---
 ALTER TABLE `pemarkiran`
   MODIFY `no` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `tarif`
---
 ALTER TABLE `tarif`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
